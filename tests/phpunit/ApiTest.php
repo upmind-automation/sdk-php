06c82de635fa16a7a120c7addb416f786f8245e7
sam-bee <130986804+sam-bee@users.noreply.github.com>
2024-08-05 22:20:34 +0100

    To provide improved test coverage, test ability to insert or override a brand ID

--- tests/phpunit/ApiTest.php
+++ tests/phpunit/ApiTest.php
@@ -28,25 +28,17 @@ class ApiTest extends TestCase
      */
     private MockObject $requestFactory;
 
+    private Config $config;
+
     protected function setUp(): void
     {
-        $config = new Config(
-            'hello-i-am-a-token',
-            null,
-            false,
-            false,
-            'api.upmind.io',
-            'https',
-            false
-        );
+        $this->config = $this->getFakeConfig();
         $logger = new NullLogger();
         $this->requestFactory = $this->getMockBuilder(RequestFactoryInterface::class)->getMock();
-
         $streamFactory = $this->getMockBuilder(StreamFactoryInterface::class)->getMock();
-
         $this->httpClient = $this->getMockBuilder(ClientInterface::class)->getMock();
 
-        $this->sut = new Api($config, $logger, $this->httpClient, $this->requestFactory, $streamFactory);
+        $this->sut = new Api($this->config, $logger, $this->httpClient, $this->requestFactory, $streamFactory);
     }
 
     public function testGetRequestSendsRequest(): void
@@ -89,6 +81,53 @@ class ApiTest extends TestCase
         $this->sut->get('/target/uri', $queryParams);
     }
 
+    public function testBrandIdFromConfigUsed(): void
+    {
+        $this->config->overrideBrandId('123');
+
+        // Request will be created
+        $request = $this->getMockBuilder(RequestInterface::class)->getMock();
+
+        // The request should have a URI the new brand ID
+        $this->requestFactory
+            ->expects($spy = $this->any())
+            ->method('createRequest')
+            ->with('GET', 'https://api.upmind.io/target/uri?brand_id=123&without_notifications=0')
+            ->willReturn($request);
+
+        // Other misc. setup
+        $this->requestHeadersWillBeSent($request);
+        $this->httpClientWillReturnResponse($request);
+
+        // Exercise subject under test
+        $this->sut->get('/target/uri');
+    }
+
+    public function testBrandIdFromConfigOverridable(): void
+    {
+        $this->config->overrideBrandId('123');
+
+        // In this test, set an override of the above default Brand ID in the query param
+        $queryParams = new QueryParams(['brand_id' => '456']);
+
+        // Request will be created
+        $request = $this->getMockBuilder(RequestInterface::class)->getMock();
+
+        // The request should have a URI the override brand ID
+        $this->requestFactory
+            ->expects($spy = $this->any())
+            ->method('createRequest')
+            ->with('GET', 'https://api.upmind.io/target/uri?brand_id=456&without_notifications=0')
+            ->willReturn($request);
+
+        // Other misc. setup
+        $this->requestHeadersWillBeSent($request);
+        $this->httpClientWillReturnResponse($request);
+
+        // Exercise subject under test
+        $this->sut->get('/target/uri', $queryParams);
+    }
+
     private function requestHeadersWillBeSent(MockObject $request): void
     {
         $request->expects($this->atLeastOnce())->method('withHeader')->willReturn($request);
@@ -98,5 +137,30 @@ class ApiTest extends TestCase
     {
         $response = $this->getMockBuilder(ResponseInterface::class)->getMock();
         $this->httpClient->expects($this->once())->method('sendRequest')->with($request)->willReturn($response);
+    }
+
+    private function getFakeConfig(): Config
+    {
+        $configVals = [
+            'hello-i-am-a-token',
+            null,
+            false,
+            false,
+            'api.upmind.io',
+            'https',
+            false
+        ];
+
+        return new class(...$configVals) extends Config {
+            private ?string $overrideBrandId = null;
+            public function overrideBrandId(string $brandId): void
+            {
+                $this->overrideBrandId = $brandId;
+            }
+            public function getBrandId(): ?string
+            {
+                return $this->overrideBrandId;
+            }
+        };
     }
 }
